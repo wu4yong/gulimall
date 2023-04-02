7101a00b1a22f0cc958111cd1b47b0f8ef881bdc
wuyong <wu4yong@163.com>
2023-04-02 15:06:56 +0800

    优惠卷单元测试

--- gulimall-coupon/src/test/java/com/atguigu/gulimall/coupon/GulimallCouponApplicationTests.java
+++ gulimall-coupon/src/test/java/com/atguigu/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -1,17 +1,29 @@
 package com.atguigu.gulimall.coupon;
 
 
+import com.atguigu.gulimall.coupon.entity.CouponEntity;
+import com.atguigu.gulimall.coupon.service.CouponService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallCouponApplicationTests {
+public class GulimallCouponApplicationTests {
+
+
+    @Autowired
+    CouponService couponService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        CouponEntity couponEntity= new CouponEntity();
+        couponEntity.setCouponName("优惠卷名字");
+        couponService.save(couponEntity);
+        System.out.println("优化卷保存成功!");
+
     }
 
 }
