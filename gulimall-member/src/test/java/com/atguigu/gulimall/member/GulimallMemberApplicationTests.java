197bd6187aca9c139fe7d749092a4a278de3f61b
wuyong <wu4yong@163.com>
2023-04-02 14:59:53 +0800

    会员单元测试

--- gulimall-member/src/test/java/com/atguigu/gulimall/member/GulimallMemberApplicationTests.java
+++ gulimall-member/src/test/java/com/atguigu/gulimall/member/GulimallMemberApplicationTests.java
@@ -1,17 +1,27 @@
 package com.atguigu.gulimall.member;
 
 
+import com.atguigu.gulimall.member.entity.UmsMemberEntity;
+import com.atguigu.gulimall.member.service.UmsMemberService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallMemberApplicationTests {
+public class GulimallMemberApplicationTests {
+    @Autowired
+    UmsMemberService umsMemberService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        UmsMemberEntity umsMemberEntity = new UmsMemberEntity();
+        umsMemberEntity.setJob("电子厂");
+        umsMemberService.save(umsMemberEntity);
+        System.out.println("添加会员成功!");
+
     }
 
 }
