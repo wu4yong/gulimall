e12ff428ead64dfe2b06d2dc8da4aba29e084571
wuyong <wu4yong@163.com>
2023-04-02 15:28:10 +0800

    仓库服务单元测试

--- gulimall-ware/src/test/java/com/atguigu/gulimall/ware/GulimallWareApplicationTests.java
+++ gulimall-ware/src/test/java/com/atguigu/gulimall/ware/GulimallWareApplicationTests.java
@@ -1,17 +1,30 @@
 package com.atguigu.gulimall.ware;
 
 
+import com.atguigu.gulimall.ware.entity.WareInfoEntity;
+import com.atguigu.gulimall.ware.service.WareInfoService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallWareApplicationTests {
+public class GulimallWareApplicationTests {
+
+    @Autowired
+    WareInfoService wareInfoService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        WareInfoEntity wareInfoEntity = new WareInfoEntity();
+        wareInfoEntity.setName("四川乐山");
+        wareInfoEntity.setAddress("井研县");
+        wareInfoService.save(wareInfoEntity);
+        System.out.println("仓库服务保存成功!");
+
+
     }
 
 }
