1c2055aafd7351700e41b17812edf9575cbfc6e4
wuyong <wu4yong@163.com>
2023-04-02 14:53:12 +0800

    订单单元测试

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -1,8 +1,9 @@
-package com.xunqi.gulimall.order;
+package com.atguigu.gulimall.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.atguigu.gulimall.order.dao")//添加mybatis文件扫码
 @SpringBootApplication
 public class GulimallOrderApplication {
 
--- gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,17 +1,30 @@
 package com.atguigu.gulimall.order;
 
-
+import com.atguigu.gulimall.order.entity.OrderEntity;
+import com.atguigu.gulimall.order.service.OrderService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class GulimallOrderApplicationTests {
+public class GulimallOrderApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    OrderService orderService;
+
+    @Test
+    public void contextLoads() {
+        OrderEntity orderEntity = new OrderEntity();
+        orderEntity.setCommentTime(new Date());
+        orderEntity.setPayAmount(new BigDecimal(100));
+        orderService.save(orderEntity);
+        System.out.println("保存订单成功!");
+    }
 
 }
