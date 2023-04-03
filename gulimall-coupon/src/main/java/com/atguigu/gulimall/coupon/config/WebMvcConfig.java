c9062fc15e6c682cf591c5bca44b559a83e6058d
wuyong <wu4yong@163.com>
2023-04-04 01:03:30 +0800

    添加配置中心代码

---
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.atguigu.gulimall.coupon.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Description 跨域访问配置
+ * @Author wuyong
+ * @Date 2023/4/4 0:49
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .maxAge(3600);
+    }
+
+}
--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +25,23 @@ import com.atguigu.common.utils.R;
  * @email wu4yong@163.com
  * @date 2023-04-02 12:27:40
  */
+@RefreshScope //动态的从配置中心读取配置
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public R getConfigInfo(){
+        return R.ok().put("name",name).put("age",age);
+    }
 
     /**
      * 列表
