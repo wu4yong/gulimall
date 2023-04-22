147998d0a70ca3b2d303ed075dbca6358fdb85ef
“wu4yong” <wu4yong@163.com>
2023-04-22 15:26:28 +0800

    添加文件上传组件

--- gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/controller/OssController.java
+++ gulimall-third-party/src/main/java/com/atguigu/gulimall/thirdparty/controller/OssController.java
@@ -5,11 +5,15 @@ import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
 import com.atguigu.common.utils.R;
+import com.atguigu.gulimall.thirdparty.service.IQiNiuService;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedHashMap;
@@ -21,6 +25,9 @@ public class OssController {
     @Autowired
     OSS ossClient;
 
+    @Autowired
+    IQiNiuService qiNiuService;
+
     @Value("${spring.cloud.alicloud.oss.endpoint}")
     private String endpoint;
     @Value("${spring.cloud.alicloud.oss.bucket}")
@@ -31,47 +38,60 @@ public class OssController {
 
 
     @RequestMapping("/oss/policy")
-    public R policy() {
-
-
-
+    public R policy() throws QiniuException {
         //https://gulimall-hello.oss-cn-beijing.aliyuncs.com/hahaha.jpg
 
-        String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
-        // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
-//        String callbackUrl = "http://88.88.88.88:8888";
-        String format = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-        String dir = format + "/"; // 用户上传文件时指定的前缀。
+//        String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
+//        // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
+//        //String callbackUrl = "http://88.88.88.88:8888";
+//        String format = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+//        String dir = format + "/"; // 用户上传文件时指定的前缀。
+//
+//        Map<String, String> respMap = null;
+//        try {
+//            long expireTime = 30;
+//            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+//            Date expiration = new Date(expireEndTime);
+//            PolicyConditions policyConds = new PolicyConditions();
+//            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+//            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+//
+//            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+//            byte[] binaryData = postPolicy.getBytes("utf-8");
+//            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+//            String postSignature = ossClient.calculatePostSignature(postPolicy);
+//
+//            respMap = new LinkedHashMap<String, String>();
+//            respMap.put("accessid", accesskey);
+//            respMap.put("policy", encodedPolicy);
+//            respMap.put("signature", postSignature);
+//            respMap.put("dir", dir);
+//            respMap.put("host", host);
+//            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+//            // respMap.put("expire", formatISO8601Date(expiration));
+//        } catch (Exception e) {
+//            // Assert.fail(e.getMessage());
+//            System.out.println(e.getMessage());
+//        }
+//        return R.ok().put("data", respMap);
 
-        Map<String, String> respMap = null;
-        try {
-            long expireTime = 30;
-            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
-            Date expiration = new Date(expireEndTime);
-            PolicyConditions policyConds = new PolicyConditions();
-            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
-            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+        String localFilePath = "D:\\app\\qiniu\\oppo.png";
+        File file = new File(localFilePath);
+        Response response = qiNiuService.uploadFile(file);
+        Map<String, String> respMap = new LinkedHashMap<>();
+        respMap.put("info", response.getInfo());
 
-            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
-            byte[] binaryData = postPolicy.getBytes("utf-8");
-            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
-            String postSignature = ossClient.calculatePostSignature(postPolicy);
+        return R.ok().put("data", respMap);
+    }
 
-            respMap = new LinkedHashMap<String, String>();
-            respMap.put("accessid", accesskey);
-            respMap.put("policy", encodedPolicy);
-            respMap.put("signature", postSignature);
-            respMap.put("dir", dir);
-            respMap.put("host", host);
-            respMap.put("expire", String.valueOf(expireEndTime / 1000));
-            // respMap.put("expire", formatISO8601Date(expiration));
+    @RequestMapping("/oss/qiniuOss")
+    public R uploadToQiNiyOss() throws QiniuException {
+        String localFilePath = "D:\\app\\qiniu\\oppo.png";
+        File file = new File(localFilePath);
+        Response response = qiNiuService.uploadFile(file);
+        Map<String, String> respMap = new LinkedHashMap<>();
+        respMap.put("info", response.getInfo());
 
-
-        } catch (Exception e) {
-            // Assert.fail(e.getMessage());
-            System.out.println(e.getMessage());
-        }
-
-        return R.ok().put("data",respMap);
+        return R.ok().put("data", respMap);
     }
 }
