d255a53717cfa987284cf9b23edb68fc9dd532ce
“wu4yong” <wu4yong@163.com>
2023-04-25 01:39:19 +0800

    减少线程池配置类

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/config/MyThreadConfig.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/config/MyThreadConfig.java
@@ -1,6 +1,6 @@
 package com.atguigu.gulimall.product.config;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,24 +8,31 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
 /**
  * @author wuyong
  * @Description: 线程池配置类
  * @email wu4yong@163.com
  * @date 2023-04-02 12:20:38
  */
-
-@EnableConfigurationProperties(ThreadPoolConfigProperties.class)
 @Configuration
 public class MyThreadConfig {
 
+    @Value("${gulimall.thread.coreSize}")
+    private Integer coreSize;
+
+    @Value("${gulimall.thread.maxSize}")
+    private Integer maxSize;
+
+    @Value("${gulimall.thread.keepAliveTime}")
+    private Integer keepAliveTime;
 
     @Bean
-    public ThreadPoolExecutor threadPoolExecutor(ThreadPoolConfigProperties pool) {
+    public ThreadPoolExecutor threadPoolExecutor() {
         return new ThreadPoolExecutor(
-                pool.getCoreSize(),
-                pool.getMaxSize(),
-                pool.getKeepAliveTime(),
+                coreSize,
+                maxSize,
+                keepAliveTime,
                 TimeUnit.SECONDS,
                 new LinkedBlockingDeque<>(100000),
                 Executors.defaultThreadFactory(),
