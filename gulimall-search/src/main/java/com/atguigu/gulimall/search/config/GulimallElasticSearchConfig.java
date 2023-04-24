6d46abbbff0aa30bf6a653a977e1f033c78a1cc5
“wu4yong” <wu4yong@163.com>
2023-04-24 23:32:14 +0800

    添加搜索模块

--- gulimall-search/src/main/java/com/atguigu/gulimall/search/config/GulimallElasticSearchConfig.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/config/GulimallElasticSearchConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- *
  * @author wuyong
  * @email wu4yong@163.com
  * @date 2023-04-02 12:29:36
@@ -16,15 +15,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GulimallElasticSearchConfig {
-
-    // @Bean
-    // public RestHighLevelClient esRestClient(){
-    //     RestHighLevelClient client = new RestHighLevelClient(
-    //             RestClient.builder(new HttpHost("192.168.137.14", 9200, "http")));
-    //     return  client;
-    // }
-
     public static final RequestOptions COMMON_OPTIONS;
+
     static {
         RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
         // builder.addHeader("Authorization", "Bearer " + TOKEN);
@@ -33,12 +25,14 @@ public class GulimallElasticSearchConfig {
         //                 .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
         COMMON_OPTIONS = builder.build();
     }
-
     @Bean
-    public RestHighLevelClient esRestClient(){
+    public RestHighLevelClient esRestClient() {
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(new HttpHost("114.115.138.227", 9200, "http")));
-        return  client;
+        return client;
     }
+
+
+
 
 }
