c425599e59929124c6c97e303feecab82f3c7570
“wu4yong” <wu4yong@163.com>
2023-04-26 01:54:29 +0800

    测试读写锁

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/web/IndexController.java
@@ -4,8 +4,11 @@ import com.atguigu.gulimall.product.entity.CategoryEntity;
 import com.atguigu.gulimall.product.service.CategoryService;
 import com.atguigu.gulimall.product.vo.Catelog2Vo;
 import org.redisson.api.RLock;
+import org.redisson.api.RReadWriteLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -32,7 +36,7 @@ public class IndexController {
     private RedissonClient redisson;
 
     @Autowired
-//    private StringRedisTemplate stringRedisTemplate;
+    private StringRedisTemplate stringRedisTemplate;
 
     @GetMapping(value = {"/", "index.html"})
     private String indexPage(Model model) {
@@ -106,50 +110,55 @@ public class IndexController {
      * 写 + 写 ：阻塞方式
      * 读 + 写 ：有读锁。写也需要等待
      * 只要有读或者写的存都必须等待
+     *
      * @return
      */
-//    @GetMapping(value = "/write")
-//    @ResponseBody
-//    public String writeValue() {
-//        String s = "";
-//        RReadWriteLock readWriteLock = redisson.getReadWriteLock("rw-lock");
-//        RLock rLock = readWriteLock.writeLock();
-//        try {
-//            //1、改数据加写锁，读数据加读锁
-//            rLock.lock();
-//            s = UUID.randomUUID().toString();
-//            ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
-//            ops.set("writeValue",s);
-//            TimeUnit.SECONDS.sleep(10);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        } finally {
-//            rLock.unlock();
-//        }
-//
-//        return s;
-//    }
+    @GetMapping(value = "/write")
+    @ResponseBody
+    public String writeValue() {
+        String s = "";
+        RReadWriteLock readWriteLock = redisson.getReadWriteLock("rw-lock");
+        RLock rLock = readWriteLock.writeLock();
+        try {
+            //1、改数据加写锁，读数据加读锁
+            rLock.lock();
+            s = UUID.randomUUID().toString();
+            ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+            ops.set("writeValue", s);
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            rLock.unlock();
+        }
 
-//    @GetMapping(value = "/read")
-//    @ResponseBody
-//    public String readValue() {
-//        String s = "";
-//        RReadWriteLock readWriteLock = redisson.getReadWriteLock("rw-lock");
-//        //加读锁
-//        RLock rLock = readWriteLock.readLock();
-//        try {
-//            rLock.lock();
-//            ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
-//            s = ops.get("writeValue");
-//            try { TimeUnit.SECONDS.sleep(10); } catch (InterruptedException e) { e.printStackTrace(); }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        } finally {
-//            rLock.unlock();
-//        }
-//
-//        return s;
-//    }
+        return s;
+    }
+
+    @GetMapping(value = "/read")
+    @ResponseBody
+    public String readValue() {
+        String s = "";
+        RReadWriteLock readWriteLock = redisson.getReadWriteLock("rw-lock");
+        //加读锁
+        RLock rLock = readWriteLock.readLock();
+        try {
+            rLock.lock();
+            ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+            s = ops.get("writeValue");
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            rLock.unlock();
+        }
+
+        return s;
+    }
 
 
     /**
