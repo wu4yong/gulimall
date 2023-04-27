a47f1850e0428434dcb766569c0c40f90d5dc39a
“wu4yong” <wu4yong@163.com>
2023-04-27 14:28:45 +0800

    Md5及颜值加密

--- gulimall-member/src/test/java/com/atguigu/gulimall/member/GulimallMemberApplicationTests.java
+++ gulimall-member/src/test/java/com/atguigu/gulimall/member/GulimallMemberApplicationTests.java
@@ -6,14 +6,11 @@ import com.atguigu.gulimall.member.service.UmsMemberService;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.codec.digest.Md5Crypt;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class GulimallMemberApplicationTests {
     @Autowired
     UmsMemberService umsMemberService;
@@ -29,16 +26,25 @@ public class GulimallMemberApplicationTests {
 
     @Test
     public void testMd5Hex() {
+        // e10adc3949ba59abbe56e057f20f883e
+        // 抗修改性，   彩虹表
         String s = DigestUtils.md5Hex("123456");
         System.out.println(s);
+
+        // MD5不能直接进行密码的加密存储
+        //盐值加密： 随机值  加盐
+        // $1$gcUUBbUS$3ecnXyPey8raKAKok.cZk1 前缀 $1$
         System.out.println(Md5Crypt.md5Crypt("123456".getBytes()));
+        // $1$qqqqqqqq$AZofg3QwurbxV3KEOzwuI1
         System.out.println(Md5Crypt.md5Crypt("123456".getBytes(), "$1$qqqqqqqq"));
 
+        // 密码加密器：
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        //$2a$10$GT0TjB5YK5Vx77Y.2N7hkuYZtYAjZjMlE6NWGE2Aar/7pk/Rmhf8S
-        //$2a$10$cR3lis5HQQsQSSh8/c3L3ujIILXkVYmlw28vLA39xz4mHDN/NBVUi
+        // $2a$10$yutF.1XL6eT8rNb.F/0/3OYeMr5dYzXK13A5t40wcOk1TqrSzSe9q
+        // $2a$10$48c/n2aWi66JwU7ZeHej.eOmk6UwcgoOAHXubrssPIvEJni.ZlVI6
         String encode = bCryptPasswordEncoder.encode("123456");
-        boolean matches = bCryptPasswordEncoder.matches("123456", "$2a$10$GT0TjB5YK5Vx77Y.2N7hkuYZtYAjZjMlE6NWGE2Aar/7pk/Rmhf8S");
+        System.out.println("123456编码结果："+encode);
+        boolean matches = bCryptPasswordEncoder.matches("123456", "$2a$10$yutF.1XL6eT8rNb.F/0/3OYeMr5dYzXK13A5t40wcOk1TqrSzSe9q");
 
         System.out.println(encode + "==>" + matches);
     }
