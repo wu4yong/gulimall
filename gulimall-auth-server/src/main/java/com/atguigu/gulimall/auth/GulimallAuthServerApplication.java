f0700412efb0a35a33134eea2cac2c5acf90b9f1
“wu4yong” <wu4yong@163.com>
2023-04-27 16:55:58 +0800

    SpringSeesion核心原理

--- gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/GulimallAuthServerApplication.java
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/GulimallAuthServerApplication.java
@@ -11,7 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * 1）、@EnableRedisHttpSession导入RedisHttpSessionConfiguration配置
  *      1、给容器中添加了一个组件
  *          RedisOperationsSessionRepository：Redis操作session，session的增删改查封装类
- *
+ *      2、SessionRepositoryFilter： session存储过滤器，每个请求过来都必须经过过滤器
  */
 
 //@EnableRedisHttpSession     //整合Redis作为session存储
