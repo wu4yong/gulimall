32326d6e7a61e3d3b99fb5bde589ee710779e861
“wu4yong” <wu4yong@163.com>
2023-04-30 21:28:28 +0800

    RabbitMQ集成

---
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/config/MyRabbitConfig.java
@@ -0,0 +1,91 @@
+package com.atguigu.gulimall.order.config;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * MyRabbitConfig配置
+ *
+ * @author wuyong
+ * @email wu4yong@163.com
+ * @date 2023-04-02 12:31:03
+ */
+@Configuration
+public class MyRabbitConfig {
+
+    private RabbitTemplate rabbitTemplate;
+
+    @Primary
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        this.rabbitTemplate = rabbitTemplate;
+        rabbitTemplate.setMessageConverter(messageConverter());
+        initRabbitTemplate();
+        return rabbitTemplate;
+    }
+
+    /**
+     * 使用JSON序列化机制，进行消息转换
+     * @return
+     */
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    /**
+     * 定制RabbitTemplate
+     * 1、服务收到消息就会回调
+     *      1、spring.rabbitmq.publisher-confirms: true
+     *      2、设置确认回调
+     * 2、消息正确抵达队列就会进行回调
+     *      1、spring.rabbitmq.publisher-returns: true
+     *         spring.rabbitmq.template.mandatory: true
+     *      2、设置确认回调ReturnCallback
+     *
+     * 3、消费端确认(保证每个消息都被正确消费，此时才可以broker删除这个消息)
+     *      1.默认是自动确认的，只要消息接收到，客户端会自动确认，服务端就会移除这个消息
+     *          问题：
+     *              我们收到很多消息，自动回复给服务器ack，只有一个消息处理成功，宕机了，发生消息丢失
+     *              消费者手动确认模式。只要我们明确告诉MQ，货物被签收，
+     *              没有ack，消息就一直是unack状态，即使Consumer宕机。
+     *              消息也不会丢失，会重新变为Read，下一次有新的Consumer连接进来就发给它
+     *       2.如何签收
+     *           channel.basicAck(deliveryTag,false); 签收：业务成功完成后就应该签收
+     *           channel.basicNack(deliveryTag,false,true); 拒签，业务失败
+     */
+    // @PostConstruct  //MyRabbitConfig对象创建完成以后，执行这个方法
+    public void initRabbitTemplate() {
+
+        /**
+         * 1、只要消息抵达Broker就ack=true
+         * correlationData：当前消息的唯一关联数据(这个是消息的唯一id)
+         * ack：消息是否成功收到
+         * cause：失败的原因
+         */
+        //设置确认回调
+        rabbitTemplate.setConfirmCallback((correlationData,ack,cause) -> {
+            System.out.println("confirm...correlationData["+correlationData+"]==>ack:["+ack+"]==>cause:["+cause+"]");
+        });
+
+
+        /**
+         * 只要消息没有投递给指定的队列，就触发这个失败回调
+         * message：投递失败的消息详细信息
+         * replyCode：回复的状态码
+         * replyText：回复的文本内容
+         * exchange：当时这个消息发给哪个交换机
+         * routingKey：当时这个消息用哪个路由键
+         */
+        rabbitTemplate.setReturnCallback((message,replyCode,replyText,exchange,routingKey) -> {
+            System.out.println("Fail Message["+message+"]==>replyCode["+replyCode+"]" +
+                    "==>replyText["+replyText+"]==>exchange["+exchange+"]==>routingKey["+routingKey+"]");
+        });
+    }
+}
--- gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderItemServiceImpl.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/service/impl/OrderItemServiceImpl.java
@@ -1,18 +1,25 @@
 package com.atguigu.gulimall.order.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
+import com.atguigu.common.utils.PageUtils;
+import com.atguigu.common.utils.Query;
+import com.atguigu.gulimall.order.dao.OrderItemDao;
+import com.atguigu.gulimall.order.entity.OrderEntity;
+import com.atguigu.gulimall.order.entity.OrderItemEntity;
+import com.atguigu.gulimall.order.entity.OrderReturnReasonEntity;
+import com.atguigu.gulimall.order.service.OrderItemService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.atguigu.common.utils.PageUtils;
-import com.atguigu.common.utils.Query;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.stereotype.Service;
 
-import com.atguigu.gulimall.order.dao.OrderItemDao;
-import com.atguigu.gulimall.order.entity.OrderItemEntity;
-import com.atguigu.gulimall.order.service.OrderItemService;
+import java.io.IOException;
+import java.util.Map;
 
 
+//@RabbitListener(queues = {"hello-java-queue"})
 @Service("orderItemService")
 public class OrderItemServiceImpl extends ServiceImpl<OrderItemDao, OrderItemEntity> implements OrderItemService {
 
@@ -24,6 +31,61 @@ public class OrderItemServiceImpl extends ServiceImpl<OrderItemDao, OrderItemEnt
         );
 
         return new PageUtils(page);
+    }
+
+
+    /**
+     * queues：声明需要监听的队列
+     * channel：当前传输数据的通道
+     *
+     * 参数类型：
+     * 1.Message message：原生消息详细信息：头+体
+     * 2.发送消息的类型
+     * 3.Channel channel：当前传输数据的通道
+     *
+     * Queue：可以很多人都来监听。只要收到消息，队列删除消息，而且只能有一个收到消息
+     *  1. 同一个消息，只能有一个客户端收到
+     *  2.只有一个消息完全处理完，方法运行结束，我们就可以收到下一个消息
+     *
+     */
+
+//    @RabbitListener(queues = {"hello-java-queue"})
+//    @RabbitHandler
+    public void revieveMessage(Message message,
+                               OrderReturnReasonEntity content,
+                               Channel channel) {
+        //拿到主体内容
+        byte[] body = message.getBody();
+        //拿到的消息头属性信息
+        MessageProperties messageProperties = message.getMessageProperties();
+        System.out.println("接受到的消息...内容" + message + "===内容：" + content);
+
+//        channel 按照顺序自增
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        System.out.println("deliveryTag="+deliveryTag);
+
+        try {
+            if (deliveryTag%2==0){
+                channel.basicAck(deliveryTag,false);
+                System.out.println("签收了货物："+deliveryTag);
+            } else {
+//                channel.basicNack();
+                System.out.println("没有签收货物："+deliveryTag);
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
+    //    @RabbitHandler
+    public void revieveMessage2(Message message,
+                                OrderEntity content) {
+        //拿到主体内容
+        byte[] body = message.getBody();
+        //拿到的消息头属性信息
+        MessageProperties messageProperties = message.getMessageProperties();
+        System.out.println("接受到的消息...内容" + message + "===内容：" + content);
+
     }
 
 }
